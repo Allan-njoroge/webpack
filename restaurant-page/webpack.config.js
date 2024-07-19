cf2f2ce41a0047698501a78e6b081ea010125404
Allan-njoroge <allandavis254@gmail.com>
2024-07-20 00:24:53 +0300

    starter files

---
+++ restaurant-page/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(js)$/,
+                use: "babel-loader"
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin()],
+}
