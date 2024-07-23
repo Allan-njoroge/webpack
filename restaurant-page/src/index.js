7653cce32637152b8aa2b490c3cf3bc256baa4cb
Allan-njoroge <allandavis254@gmail.com>
2024-07-23 22:45:10 +0300

    header section of the homepage complete

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,1 +1,21 @@
 import "./style.css";
+import homePage from './pages/home'
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const contactBtn = document.querySelector("#contact-btn");
+
+homePage()
+
+homeBtn.addEventListener("click", () => {
+    alert(`Home button clicked`);
+    homePage()
+})
+
+menuBtn.addEventListener("click", () => {
+    alert(`Menu button clicked`);
+})
+
+contactBtn.addEventListener("click", () => {
+    alert(`Contact button clicked`);
+})
