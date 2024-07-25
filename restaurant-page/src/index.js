09983ad7856855ad2108f9135206e479e6fc1d10
Allan-njoroge <allandavis254@gmail.com>
2024-07-25 22:08:45 +0300

    added the reserve and contact pages

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,12 +1,14 @@
 import "./style.css";
 import homePage from './pages/home'
 import menuPage from './pages/menu'
+import contactPage from './pages/contact'
+import reservePage from './pages/reserve'
 
 const homeBtn = document.querySelector("#home-btn");
 const menuBtn = document.querySelector("#menu-btn");
 const contactBtn = document.querySelector("#contact-btn");
 
-
+homePage()
 
 homeBtn.addEventListener("click", () => {
     alert(`Home button clicked`);
@@ -18,6 +20,12 @@ menuBtn.addEventListener("click", () => {
     menuPage()
 })
 
+resrverBtn.addEventListener("click", () => {
+    alert(`Reserve button clicked`);
+    reservePage()
+})
+
 contactBtn.addEventListener("click", () => {
     alert(`Contact button clicked`);
+    contactPage()
 })
